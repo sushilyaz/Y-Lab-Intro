17ada2f05ed7aa0603648a9e99da0ee011cbe920
suhoi <suhoio2k00@gmail.com>
2024-02-25 22:59:56 +0300

    add logg module

---
+++ logg/src/main/java/org/example/logg/LoggApplication.java
@@ -0,0 +1,13 @@
+package org.example.logg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggApplication.class, args);
+    }
+
+}
